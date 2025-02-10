bcb2f42471ad140c38bc0d608ecee4f6e7a54b49
Juliet Akani Ngomani <119965940+Juliee23@users.noreply.github.com>
2025-02-10 23:18:29 +0200

    Word dictionary

---
+++ Wordearch-main/README.md
@@ -0,0 +1,38 @@
+# Word Dictionary
+A dictionary app 
+
+<h2>Current Challenge</h2>
+<ul>
+  <li>I can't figure out why I can't access all words, even thou they are returned in object form in the console</li>
+  <li>To display images of word that has been searched</li>
+  <li>Might need to use another API</li>
+</ul>
+
+<h2>Colour Scheme</h2>
+<ul>
+  <li>indigo-500</li>
+  <li>gray-500</li>
+  <li>bg-gray-800</li>
+</ul>
+
+<h2>Screenshot</h2>
+<img src="">
+
+<h2>Built with:</h2>
+<ul>
+  <li>HTML</li>
+  <li>Tailwind CSS</li>
+  <li>JavaScript</li>
+</ul>
+
+<h2>Demo</h2>
+<ul>
+  <li><a href="https://.netlify.app">.netlify.app</a></li>
+</ul>
+
+<h2>Find Me</h2>
+<ul>
+  <li><a href="https://hashnode.com/leannacodes">Hashnode @Juliet</a></li>
+  <li><a href="https://www.linkedin.com/in/juliet-akani-ngomani-278294157/">Linkedin @Juliet</a></li>
+  <li><a href="https://codepen.io/Juliee23">Codepen @Juliee23</a></li>
+</ul>
---
+++ Wordearch-main/script.js
@@ -0,0 +1,47 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            // display results in the DOM
+            result.innerHTML = `
+                <div class="text-center pt-2 pb-2">
+                    <h3 class="text-3xl text-indigo-500 capitalize underline">${inpWord}</h3>
+                    <p class="text-white text-xl">/${data[0].phonetic}/</p>
+                </div>
+
+                <div class="py-8 flex flex-wrap md:flex-nowrap">
+                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                        <span class="font-semibold title-font text-indigo-500">${data[0].meanings[0].partOfSpeech}</span>
+                    </div>
+                    <div class="md:flex-grow text-gray-200">
+                        <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">2. ${data[0].meanings[0].definitions[1].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">3. ${data[0].meanings[0].definitions[2].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">4. ${data[0].meanings[0].definitions[3].definition || ""}</p>
+                        <p class="mt-5 leading-relaxed">5. ${data[0].meanings[0].definitions[4].definition || ""}</p>
+                    </div>
+                </div>
+               
+                <hr class="border-solid border-1 border-indigo-500">
+                <div class="py-8 flex flex-wrap md:flex-nowrap">
+                    <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                        <span class="font-semibold title-font text-indigo-500">Example</span>
+                    </div>
+                    <div class="md:flex-grow text-gray-200">
+                        <p class="leading-relaxed">1. ${data[0].meanings[0].definitions[0].example || "Not available"}</p>
+                    </div>
+                </div>`
+        })
+
+        // error handling
+        .catch(() => {
+            result.innerHTML = `<h3 class="error text-center text-2xl text-gray-500 mt-10">I can't access the word 🥺</h3>`;
+        });
+});
